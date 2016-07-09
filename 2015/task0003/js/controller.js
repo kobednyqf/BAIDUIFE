8db1afb84c9191a64aa4f0d1fb23b010133c4a6a
唐伟豪 <redorgreen@sina.cn>
2016-07-09 18:51:38 +0800

    ife2015

--- 2015/task0003/js/controller.js
+++ 2015/task0003/js/controller.js
@@ -1,267 +1,68 @@
-//controller.js
-var controller = function Controller()
-{
-	this.bagFlag = undefined;
-	this.childFlag = undefined;
-}
-//mvc
-//
-//c
-//得到bagflag
-controller.prototype.getBagFlag = function(node)
-{
-	this.bagFlag = node;
-	console.log("bag="+this.bagFlag);
-}
+var controller = function(){};
 
-//得到childflag
-controller.prototype.getChildFlag = function(node)
-{
-	this.childFlag = node;
-	console.log("child="+this.childFlag);
-}
+controller.prototype = {
 
-//c传递v至m
-//传递bag到JSON
-controller.prototype.bagToJSON = function()
-{
-	var input = list.getElementsByClassName("task");
-	for(var i in localJSON)
-		mo.editBagTopic(i,input[i].value);
-}
+	//note全读取
+	read: function(){
+		var note = m.note;
+		list.innerHTML = '';
+		for(var i in note){
+			var task = document.createElement('div');
+			task.innerHTML = '<div class="task-pa relative"><input class="task" type="text" value="'+note[i].name+'" placeholder="TODO" maxlength="15"><a class="deleter" href="javascript:void(0)"></a></div>';
+			list.appendChild(task)
+		}
+	},
 
-//传递child到JSON
-controller.prototype.childToJSON = function(node)
-{
-	var input = children.getElementsByClassName("task");
-	for(var i in localJSON[node]["children"])
-	{
-		mo.editChildTopic(node,i,input[i].value);
-	}
-}
+	//note全写入
+	write: function(){
+		var inputs = list.getElementsByTagName('input');
+		var obj = {};
+		for(var i=0;i < inputs.length;i++){
+			obj[i] = {
+				name: inputs[i].value,
+				content: ''
+			}
+		}
+		m.note = obj;
+	},
 
-//传递desc timming到JSON
-controller.prototype.contentToJSON = function(node,childnode)
-{
-	mo.editChildTimming(node,childnode,this.regTimming(timming.value));//加入Timming正则判断
-	mo.editChildDesc(node,childnode,desc.value);
-}
+	trimTask: function(obj){
+		var newObj = {};
+		var k = 0;
+		for(var i in obj){
+			if(obj[i].name == ""){
+				continue;
+			}
+			newObj[k++] = obj[i];
+		}
+		return newObj;
+	},
 
-//所有的add delete等涉及数组重排操作的均触发保存后重新渲染
-//事件
-//事件代理
-controller.prototype.dg = function()
-{
-	var inputLeft = list.getElementsByClassName("task");
-	var inputCentre = children.getElementsByClassName("task");
-	var xLeft = list.getElementsByClassName("deleter");
-	var xRight = children.getElementsByClassName("deleter");
-	//代理判空
-	//bagFlag判空
-	if(this.bagFlag===undefined)
-	{
-		console.log("bag=undefined");
-		for(var x=0;x<inputCentre.length;x++)
-			inputCentre[x].disabled="disabled";
-	}
-	else
-	{
-		for(var x=0;x<inputCentre.length;x++)
-			inputCentre[x].disabled="";
-	}
+	save: function(){
+		localStorage.clear();
+		localStorage.note = JSON.stringify(this.trimTask(m.note));
+	},
 
-	//childFlag判空
-	if(this.childFlag===undefined)
-	{
-		console.log("child=undefined");
-		timming.disabled="disabled";
-		desc.disabled="disabled";
-	}
-	else
-	{
-		timming.disabled="";
-		desc.disabled="";
+	plus: function(){
+		var task = document.createElement('div');
+		task.innerHTML = '<div class="task-pa relative"><input class="task" type="text" value="'+''+'" placeholder="TODO" maxlength="15"><a class="deleter" href="javascript:void(0)"></a></div>';
+		list.appendChild(task);
 	}
 
 
-	//事件代理部分
-	//bag事件代理
-	for(var i=0;i<inputLeft.length;i++)
-	{
-		//代理bagflag获取+children显示
-		inputLeft[i].onclick = (function()
-		{
-			var e = i;
-			return function()
-			{
-				con.childFlag=undefined;//点击bag后childFlag置空
-				con.getBagFlag(e);
-				v.clearCentreRight(e);
-				v.showCentre(e);
-				//并且重新加载一次代理
-				con.dg();
-				v.hideBagX(e);
-				v.hideChildX(con.bagFlag,e);
-				v.viewBagX(e);
-			};
-		})(i);
+}
 
+var c = new controller;
+
+window.onload = function(){
+	if(!localStorage.note){
+		localStorage.note = JSON.stringify({0:{name:'我的第一个任务'}})
 	}
-
-	//child事件代理
-	for(var j=0;j<inputCentre.length;j++)
-	{
-		//代理childflag获取+timming/desc显示
-		inputCentre[j].onclick = (function()
-		{
-			var e = j;
-			return function()
-			{
-				con.getChildFlag(e);
-				v.clearContent();
-				v.printChildContent(con.bagFlag,e);
-				con.dg();
-				//v.hideBagX(e);
-				v.hideChildX(con.bagFlag,e)
-				v.viewChildX(con.bagFlag,e)
-			};
-		})(j);
-
-	}
-
-	//bagX事件代理
-	for(var k=0;k<xLeft.length;k++)
-	{
-		xLeft[k].onclick = function(){con.minusBag()};
-	}
-
-	//childX事件代理
-	for(var l=0;l<xRight.length;l++)
-	{
-		xRight[l].onclick = function(){con.minusChild()};
-	}
-
+	m.note = JSON.parse(localStorage.note);
+	c.read();
 }
 
-//判断长度
-controller.prototype.judgeLength = function()
-{
-	task_Len.innerHTML = "所有任务"+"("+list.getElementsByClassName("task-pa").length+")";
+window.onkeyup = function(){
+	c.write();
+	c.save();
 }
-
-//增加bag(JSON+VIEW)
-controller.prototype.plusBag = function()
-{
-	if(this.bagFlag===undefined)
-		this.bagFlag = mo.len()-1;//直接置最后一位
-	//先增加JSON部分
-	mo.insertNewBag(this.bagFlag);
-	mo.save();
-	//清Flag
-	this.childFlag=undefined;
-	this.bagFlag++;
-	//重加载
-	v.refreshBag();
-}
-
-//删除bag(JSON+VIEW)
-controller.prototype.minusBag = function()
-{
-	if(this.bagFlag===undefined)
-		return false;
-	//先增加JSON部分
-	mo.removeBag(this.bagFlag);
-	mo.save();
-	//清Flag
-	this.childFlag=undefined;
-	this.bagFlag=undefined;
-	//重加载
-	v.refreshBag();
-}
-
-
-//增加Child(JSON+VIEW)
-controller.prototype.plusChild = function()
-{
-	if(this.bagFlag===undefined)
-		return false;
-	if(this.childFlag===undefined)
-		this.childFlag = mo.childLen(this.bagFlag)-1;//直接置最后一位
-	//先增加JSON部分
-	mo.insertNewChild(this.bagFlag,this.childFlag);
-	mo.save();
-	//清childFlag
-	this.childFlag++;
-	//重加载
-	v.refreshChild();
-}
-
-//删除Child(JSON+VIEW)
-controller.prototype.minusChild = function()
-{
-	if(this.bagFlag===undefined)
-		return false;
-	if(this.childFlag===undefined)
-		return false;
-	//先增加JSON部分
-	mo.removeChild(this.bagFlag,this.childFlag);
-	mo.save();
-	//清childFlag
-	this.childFlag=undefined;
-	//重加载
-	v.refreshChild();
-}
-
-controller.prototype.regTimming = function(string)
-{
-	var reg=/^[2][0][0-9][0-9][-/.][0-1][0-9][-/.][0-3][0-9]$/;
-	if(reg.test(string)===false)
-		string = "YY-MM-DD";
-	return string
-}
-
-//oo
-var con = new controller();
-
-
-//test
-window.onload=function()
-{
-	mo.read();
-	if(localStorage.length<1){
-		localJSON[0]=cloneObject(normalBag);
-		mo.insertNewBag(0);
-		mo.insertNewBag(0);
-		mo.insertNewBag(0);
-		mo.insertNewChild(0,0);
-		mo.insertNewChild(0,0);
-		mo.insertNewChild(0,0);
-		mo.editBagTopic(0,"THIS IS TODO");
-		mo.editBagTopic(2,"SET YOUR TODO");
-		mo.editChildTopic(0,0,"THIS IS YOUR TODO");
-		mo.editChildTopic(0,2,"BY@MELO");
-		mo.editChildTimming(0,0,"2016.2")
-		mo.editChildDesc(0,0,"WRITE YOUR DESCRIBE HERE")
-	}
-
-	console.log(localJSON);
-	v.copyBagDiv();
-	v.printBag();
-	con.dg();
-	con.judgeLength();
-}
-
-doc.onkeyup=function()
-{
-	con.bagToJSON();
-	//
-	if(con.bagFlag!==undefined)
-		con.childToJSON(con.bagFlag);
-	//
-	if(con.childFlag!==undefined)
-		con.contentToJSON(con.bagFlag,con.childFlag);
-	v.changeSave();
-}
-
-
-
